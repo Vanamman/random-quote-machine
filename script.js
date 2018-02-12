23bbba427f260e6b95861db2e8025c093e50615a
vanamman <vanamman06@gmail.com>
2018-02-11 19:25:23 -0600

    Added random color on page load

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var colors = [];
+var colors = [" #9b59b6", "#2980b9", "#27ae60", "#cb4335", "#17a589", "#b7950b", "#d35400"];
 
 //change background color of page when button is clicked.
 //make quote, tweet button, and new quote button mirror the page color
+
+$(document).ready(function() {
+    colors = colors[Math.floor(colors.length * Math.random())];
+    $("body").css("background-color", colors);
+    $('button').css("background-color", colors);
+    $('#tweet').css("color", colors);
+    $('#quote').css("color", colors);
+    $('#author').css("color", colors);
+});
