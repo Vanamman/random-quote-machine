5618d004a127a099c4e81b903bf202b7133253cd
vanamman <vanamman06@gmail.com>
2018-02-15 20:40:09 -0600

    Tidied up the code

--- script.js
+++ script.js
@@ -1,48 +1,52 @@
-var colors = [" #9b59b6", "#2980b9", "#27ae60", "#cb4335", "#17a589", "#b7950b", "#d35400"];
+var colors = [
+	" #9b59b6",
+	"#2980b9",
+	"#27ae60",
+	"#cb4335",
+	"#17a589",
+	"#b7950b",
+	"#d35400"
+];
 
-$(document).ready(function() 
-{
+$(document).ready(function() {
+	var quote = "";
+	var author = "";
 
-    var quote = '';
-    var author = '';
+	function getQuote() {
+		$.ajax({
+			url: "https://random-quote-generator.herokuapp.com/api/quotes/random",
+			dataType: "json",
+			success: function(results) {
+				console.log(results);
+				$("#quote").text(results.quote);
+				$("#author").text(results.author);
+				quote = results.quote;
+				author = results.author;
+				{
+					$("#tweet").attr(
+						"href",
+						"https://twitter.com/intent/tweet?text=" +
+							encodeURIComponent('"' + quote + '" - ' + author)
+					);
+				}
+			}
+		});
+	}
 
-    function getQuote() 
-    {
-        $.ajax({
-            url: "https://random-quote-generator.herokuapp.com/api/quotes/random",
-            dataType: 'json',
-            success: function(results){
-                console.log(results);
-                $("#quote").text(results.quote);
-                $("#author").text(results.author);
-                quote = results.quote;
-                author = results.author;
-                {
-                    $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + quote + '" - ' + author));
-                }
-            }
-        });
-    }
+	function colorChange() {
+		var color = colors[Math.floor(colors.length * Math.random())];
+		$("body").css("background-color", color);
+		$("button").css("background-color", color);
+		$("#tweet").css("color", color);
+		$("#quote").css("color", color);
+		$("#author").css("color", color);
+	}
 
- 
+	$("button").click(function() {
+		getQuote();
+		colorChange();
+	});
 
-    function colorChange() 
-    {
-        var color = colors[Math.floor(colors.length * Math.random())];
-        $("body").css("background-color", color);
-        $('button').css("background-color", color);
-        $('#tweet').css("color", color);
-        $('#quote').css("color", color);
-        $('#author').css("color", color);
-    }
-
-    $('button').click(function() 
-    {
-        getQuote();
-        colorChange();
-    });
-
-    colorChange();
-    getQuote();
-
+	colorChange();
+	getQuote();
 });
