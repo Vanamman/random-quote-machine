114195d07f5666c6279d9da121111abdaddb5800
vanamman <vanamman06@gmail.com>
2018-02-15 20:31:36 -0600

    Added tweet button

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 var colors = [" #9b59b6", "#2980b9", "#27ae60", "#cb4335", "#17a589", "#b7950b", "#d35400"];
 
-
-
-function parseQuote(response) 
-{
-    console.log(response);
-    $("#quote").text(response.quoteText);
-    $("#author").text(response.quoteAuthor);
-}
-
 $(document).ready(function() 
 {
+
+    var quote = '';
+    var author = '';
+
     function getQuote() 
     {
         $.ajax({
-            url: "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=parseQuote",
-            method: 'GET',
-            dataType: "jsonp",
+            url: "https://random-quote-generator.herokuapp.com/api/quotes/random",
+            dataType: 'json',
+            success: function(results){
+                console.log(results);
+                $("#quote").text(results.quote);
+                $("#author").text(results.author);
+                quote = results.quote;
+                author = results.author;
+                {
+                    $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + quote + '" - ' + author));
+                }
+            }
         });
-}
+    }
 
-    getQuote();
+ 
 
-
-    
     function colorChange() 
     {
         var color = colors[Math.floor(colors.length * Math.random())];
@@ -33,11 +35,14 @@ $(document).ready(function()
         $('#quote').css("color", color);
         $('#author').css("color", color);
     }
-    colorChange();
 
     $('button').click(function() 
     {
         getQuote();
         colorChange();
     });
+
+    colorChange();
+    getQuote();
+
 });
